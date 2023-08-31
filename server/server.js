4d6d780856ff06d1d9c43efddbe0407a40af6807
Shubham Jain <shubhamjain3cs@gmail.com>
2023-08-31 15:27:23 +0530

    configured cors

--- server/server.js
+++ server/server.js
@@ -1,47 +1,53 @@
-const express = require('express'); 
-const app = express(); 
+const express = require('express');
+const app = express();
 const dotEnv = require('dotenv');
-const mongoose = require('mongoose'); 
-const cors = require('cors'); 
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 // configuring cors 
-app.use(cors()); 
+app.use(
+    cors({
+        allowedHeaders: "*",
+        allowMethods: "*",
+        origin: "*",
+    })
+);
 
 // configure express to receive form data 
 
 app.use(express.json(
-    
-)); 
+
+));
 
 // configure dotenv 
 
-dotEnv.config({path : './.env'}); 
+dotEnv.config({ path: './.env' });
 
-const hostname = process.env.LOCAL_HOST_NAME; 
-const port = process.env.LOCAL_PORT; 
+const hostname = process.env.LOCAL_HOST_NAME;
+const port = process.env.LOCAL_PORT;
 
 // connect to mongoDB 
 
-mongoose.connect(process.env.MONGO_DB_URL).then( (response) => {
+mongoose.connect(process.env.MONGO_DB_URL).then((response) => {
     console.log(`Connected to MongoDB successsfully`)
-}).catch( (error) => {
-    console.error(error); 
-    process.exit(1); 
+}).catch((error) => {
+    console.error(error);
+    process.exit(1);
 })
 
 
 // basic request 
 
 app.get('/', (request, response) => {
-    response.send(`<h2>Welcome to Events Booking Application</h2>`); 
-}); 
+    response.send(`<h2>Welcome to Events Booking Application</h2>`);
+});
 
 // router configuration 
 
-app.use('/api/users', require('./Router/UserRouter')); 
-app.use('/api/events', require('./Router/EventsRouter')); 
+app.use('/api/users', require('./Router/UserRouter'));
+app.use('/api/events', require('./Router/EventsRouter'));
 
 app.listen(port, hostname, () => {
-    console.log(`Express Server is started at http://${hostname}:${port}`); 
+    console.log(`Express Server is started at http://${hostname}:${port}`);
 }); 
